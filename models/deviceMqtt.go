4fb09f762a6f05ce2ae1f60f29f8ed545b35b915
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2022-12-28 17:13:18 +0100

    refactor: reformat code with .editorconfig file
    Signed-off-by: Stefano Cappa <stefano.cappa.ks89@gmail.com>

--- models/deviceMqtt.go
+++ models/deviceMqtt.go
@@ -1,10 +1,10 @@
 package models
 
 type Values struct {
-  Uuid        string `json:"uuid"`
-  ApiToken    string `json:"apiToken"`
-  On          bool   `json:"on"`
-  Temperature int    `json:"temperature"`
-  Mode        int    `json:"mode"`
-  FanSpeed    int    `json:"fanSpeed"`
+	Uuid        string `json:"uuid"`
+	ApiToken    string `json:"apiToken"`
+	On          bool   `json:"on"`
+	Temperature int    `json:"temperature"`
+	Mode        int    `json:"mode"`
+	FanSpeed    int    `json:"fanSpeed"`
 }
