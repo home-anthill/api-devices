827cd7a693c1dc775465dfd9468ed36295de523f
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2025-04-27 12:34:42 +0200

    fix: rename proto packages + change proto command order in makefile
    Signed-off-by: Stefano Cappa <stefano.cappa.ks89@gmail.com>

--- api/device/device.pb.go
+++ api/device/device.pb.go
@@ -358,7 +358,7 @@ const file_api_device_device_proto_rawDesc = "" +
 	"\amessage\x18\x02 \x01(\tR\amessage2\x84\x01\n" +
 	"\x06Device\x12<\n" +
 	"\tGetStatus\x12\x15.device.StatusRequest\x1a\x16.device.StatusResponse\"\x00\x12<\n" +
-	"\tSetValues\x12\x15.device.ValuesRequest\x1a\x16.device.ValuesResponse\"\x00B4Z2github.com/Ks89/air-conditioner/api-devices/deviceb\x06proto3"
+	"\tSetValues\x12\x15.device.ValuesRequest\x1a\x16.device.ValuesResponse\"\x00B1Z/github.com/Ks89/home-anthill/api-devices/deviceb\x06proto3"
 
 var (
 	file_api_device_device_proto_rawDescOnce sync.Once
